65a39eadf410820d7a9d424019dbd880b2db3bfb
LOU Xun <aquarhead@ela.build>
2019-01-18 17:17:23 +0000

    Show iTunes track info on menu bar

---
+++ hammerspoon.lua
@@ -0,0 +1,24 @@
+itunesTitle = hs.menubar.new()
+
+function setItunesTitle()
+  if hs.itunes.isRunning() then
+    title = hs.itunes.getCurrentTrack()
+    album = hs.itunes.getCurrentAlbum()
+    artist = hs.itunes.getCurrentArtist()
+    if title and hs.itunes.getPlaybackState() ~= hs.itunes.state_stopped then
+      itunesTitle:setTitle(title .. ' - ' .. artist .. ' | ' .. album)
+    else
+      itunesTitle:setTitle('-')
+    end
+  else
+    itunesTitle:setTitle('> iTunes')
+  end
+end
+
+function openItunes()
+  hs.application.launchOrFocusByBundleID("com.apple.iTunes")
+end
+itunesTitle:setClickCallback(openItunes)
+
+setItunesTitle()
+always = hs.timer.doEvery(3, setItunesTitle)
